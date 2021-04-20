acf9047f7e7af751f544f8e6e6693eb0a1c384c5
Armbit_thtj <armthtj1@gmail.com>
2021-04-20 15:50:55 +0700

    node typescript frame

---
+++ app/routes/index.route.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2020 Depwhite Software
+ *
+ * This file is part of the Depwhite Software project.
+ *
+ * Depwhite Software project can not be copied and/or distributed without the express
+ */
+
+"use strict"
+
+/**
+ * Load app configurations
+ */
+
+import mailRoute from "./mail.route"
+
+import { APIURL } from "../constants/config"
+export default (app) => {
+  mailRoute(`${APIURL}/mail`, app)
+}
